29eca59a1f50235a91660cd2a50183a09ae437ed
Sanchit Bhatnagar <dev.sanchitb23@gmail.com>
2023-02-07 18:42:19 +0530

    Updated contactInfo click

--- src/app/(home)/contact-us/contactInfo.tsx
+++ src/app/(home)/contact-us/contactInfo.tsx
@@ -1,38 +1,35 @@
-import { Box, Button, useClipboard, VStack } from '@chakra-ui/react'
+import { Box, useClipboard, VStack } from '@chakra-ui/react'
+import NextLink from 'next/link'
 import React from 'react'
 import { MdEmail, MdLocationOn, MdPhone } from 'react-icons/md'
 import { email, locationMapUrl, locationName, phone } from '@/constants'
 import ContactInfoBtn from './contactInfoBtn'
 
 const ContactInfo = () => {
-	const { hasCopied, onCopy } = useClipboard(email)
+	const { hasCopied: hasCopiedEmail, onCopy: onCopyEmail } = useClipboard(email)
+	const { hasCopied: hasCopiedPhone, onCopy: onCopyPhone } = useClipboard(phone)
 	return (
 		<Box py={{ base: 5, sm: 5, md: 8, lg: 10 }}>
 			<VStack pl={0} spacing={3} alignItems='flex-start' justifyContent={'flex-start'}>
 				<ContactInfoBtn
-					label={hasCopied ? 'Email Copied!' : 'Copy Email'}
+					label={hasCopiedPhone ? 'Phone No. Copied!' : 'Copy Phone No.'}
 					leftIcon={<MdPhone color='#1970F1' size='20px' />}
-					onClick={onCopy}
+					onClick={onCopyPhone}
 				>
 					{phone}
 				</ContactInfoBtn>
-				<ContactInfoBtn leftIcon={<MdEmail color='#1970F1' size='20px' />} label={'Test'}>
+				<ContactInfoBtn
+					leftIcon={<MdEmail color='#1970F1' size='20px' />}
+					label={hasCopiedEmail ? 'Email Copied!' : 'Copy Email'}
+					onClick={onCopyEmail}
+				>
 					{email}
 				</ContactInfoBtn>
-				<Button
-					px={2}
-					size='md'
-					height='48px'
-					w={'max'}
-					variant='ghost'
-					_hover={{ border: '2px solid #1C6FEB' }}
-					leftIcon={<MdLocationOn color='#1970F1' size='20px' />}
-					as='a'
-					href={locationMapUrl}
-					justifyContent={'flex-start'}
-				>
-					{locationName}
-				</Button>
+				<ContactInfoBtn label={'Open Maps'} leftIcon={<MdLocationOn color='#1970F1' size='20px' />}>
+					<NextLink href={locationMapUrl} rel={'noopener noreferrer'} target='_blank'>
+						{locationName}
+					</NextLink>
+				</ContactInfoBtn>
 			</VStack>
 		</Box>
 	)
