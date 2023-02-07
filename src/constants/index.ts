8d90e884fd0648d7b9a82e91686e0b3fce3db341
Sanchit Bhatnagar <dev.sanchitb23@gmail.com>
2023-02-08 02:45:12 +0530

    Created Axios Service

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,3 +3,9 @@ export const locationName = 'Himachal Bhawan'
 export const email = 'example@example.com'
 export const phone = '+91-988888888'
 export const clubNameShort = 'dtm'
+export const API_ENDPOINTS = {
+	BASE_URL: '',
+}
+export const SESSION_STORAGE_KEYS = {
+	USER_TOKEN: 'USER_TOKEN',
+}
