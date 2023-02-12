c348672e342eb1fae65005251f421e61e70c5549
Sanchit Bhatnagar <dev.sanchitb23@gmail.com>
2023-02-12 14:05:42 +0530

    Added some UI in About Page
    Signed-off-by: Sanchit Bhatnagar <dev.sanchitb23@gmail.com>

---
+++ src/app/(home)/about/tmFeatures.component.tsx
@@ -0,0 +1,51 @@
+'use client'
+import { Box, Flex, Heading, Icon, SimpleGrid, Stack, Text } from '@chakra-ui/react'
+import { FcAssistant, FcDonate, FcInTransit } from 'react-icons/fc'
+import type { ReactElement } from 'react'
+
+interface FeatureProps {
+	title: string
+	text: string
+	icon: ReactElement
+}
+
+const Feature = ({ title, text, icon }: FeatureProps) => (
+	<Stack>
+		<Flex w={16} h={16} align={'center'} justify={'center'} color={'white'} rounded={'full'} bg={'gray.100'} mb={1}>
+			{icon}
+		</Flex>
+		<Text fontWeight={600}>{title}</Text>
+		<Text color={'gray.600'}>{text}</Text>
+	</Stack>
+)
+
+export default function TmFeatures() {
+	return (
+		<Box px={32}>
+			<Heading>Features</Heading>
+			<SimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
+				<Feature
+					icon={<Icon as={FcAssistant} w={10} h={10} />}
+					title={'Lifetime Support'}
+					text={
+						'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
+					}
+				/>
+				<Feature
+					icon={<Icon as={FcDonate} w={10} h={10} />}
+					title={'Unlimited Donations'}
+					text={
+						'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
+					}
+				/>
+				<Feature
+					icon={<Icon as={FcInTransit} w={10} h={10} />}
+					title={'Instant Delivery'}
+					text={
+						'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
+					}
+				/>
+			</SimpleGrid>
+		</Box>
+	)
+}
